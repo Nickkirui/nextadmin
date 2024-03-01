db0fe3a0ecd4af74f77dff340e726463ce730d81
Nickkirui <nickkirui69@gmail.com>
2024-03-01 07:48:55 +0300

    Admin Dashboard

---
+++ app/ui/dashboard/transactions/transactions.jsx
@@ -0,0 +1,106 @@
+import Image from "next/image";
+import styles from "./transactions.module.css";
+
+const Transactions = () => {
+  return (
+    <div className={styles.container}>
+      <h2 className={styles.title}>Latest Transactions</h2>
+      <table className={styles.table}>
+        <thead>
+          <tr>
+            <td>Name</td>
+            <td>Status</td>
+            <td>Date</td>
+            <td>Amount</td>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <div className={styles.user}>
+                <Image
+                  src="/noavatar.png"
+                  alt=""
+                  width={40}
+                  height={40}
+                  className={styles.userImage}
+                />
+                John Makori
+              </div>
+            </td>
+            <td>
+              <span className={`${styles.status} ${styles.pending}`}>
+                Pending
+              </span>
+            </td>
+            <td>14.02.2024</td>
+            <td>KSH 35600</td>
+          </tr>
+          <tr>
+            <td>
+              <div className={styles.user}>
+                <Image
+                  src="/noavatar.png"
+                  alt=""
+                  width={40}
+                  height={40}
+                  className={styles.userImage}
+                />
+                David Kariuki
+              </div>
+            </td>
+            <td>
+              <span className={`${styles.status} ${styles.done}`}>Done</span>
+            </td>
+            <td>14.02.2024</td>
+            <td>Ksh42200</td>
+          </tr>
+          <tr>
+            <td>
+              <div className={styles.user}>
+                <Image
+                  src="/noavatar.png"
+                  alt=""
+                  width={40}
+                  height={40}
+                  className={styles.userImage}
+                />
+                Brian Onyango
+              </div>
+            </td>
+            <td>
+              <span className={`${styles.status} ${styles.cancelled}`}>
+                Cancelled
+              </span>
+            </td>
+            <td>14.02.2024</td>
+            <td>KSH34200</td>
+          </tr>
+          <tr>
+            <td>
+              <div className={styles.user}>
+                <Image
+                  src="/noavatar.png"
+                  alt=""
+                  width={40}
+                  height={40}
+                  className={styles.userImage}
+                />
+                Willy Sang
+              </div>
+            </td>
+            <td>
+              <span className={`${styles.status} ${styles.pending}`}>
+                Pending
+              </span>
+            </td>
+            <td>14.02.2024</td>
+            <td>KS45200</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default Transactions;
